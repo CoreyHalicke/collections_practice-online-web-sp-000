17f2a52505e612d8cd4251f73b9afb4cd7fd5342
Corey H <c.l.halicke@gmail.com>
2019-03-25 16:11:44 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,9 +30,9 @@ describe 'collections practice' do
     end
   end
 
-  describe '#swap_elements' do
-    it 'swap the second and third elements of an array' do
-      expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
+  describe '#swap_elements_from_to' do
+    it 'swap two elements in an array based on argument' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
 
